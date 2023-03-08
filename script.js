14f6d50b72cc9e7b9e888e46bc6f957fc3d03f0e
Maxim Vorobei <85136737+MaxVorobei@users.noreply.github.com>
2023-03-08 12:59:11 +0100

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+var swiper = new Swiper('.blog-slider', {
+
+   spaceBetween: 30,
+   effect: 'fade',
+   loop: true,
+   mousewheel: {
+      invert: false,
+   },
+   pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+   }
+});
